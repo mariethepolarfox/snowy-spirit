c3d6f1e2c4976a36d42167fef4b2e0b68becbbc3
Marie <80259183+mariethepolarfox@users.noreply.github.com>
2026-04-20 15:02:02 +0200

    thingy

--- versions/1.21.11/src/main/kotlin/me/marie/snowyspirits/screen/ConfigScreen.kt
+++ versions/1.21.11/src/main/kotlin/me/marie/snowyspirits/screen/ConfigScreen.kt
@@ -1,10 +1,7 @@
 package me.marie.snowyspirits.screen
 
 import earth.terrarium.olympus.client.components.Widgets
-import earth.terrarium.olympus.client.components.base.BaseWidget
 import earth.terrarium.olympus.client.components.base.ListWidget
-import earth.terrarium.olympus.client.components.base.renderer.WidgetRenderer
-import earth.terrarium.olympus.client.components.base.renderer.WidgetRendererContext
 import earth.terrarium.olympus.client.components.compound.radio.RadioState
 import earth.terrarium.olympus.client.components.dropdown.DropdownState
 import earth.terrarium.olympus.client.components.renderers.WidgetRenderers
@@ -16,10 +13,7 @@ import earth.terrarium.olympus.client.utils.Orientation
 import me.marie.snowyspirits.SnowySpirits
 import me.marie.snowyspirits.SnowySpirits.mc
 import me.marie.snowyspirits.config.Config
-import me.marie.snowyspirits.screen.widget.SliderWidget
-import me.marie.snowyspirits.screen.widget.UnlabeledSliderWidget
 import net.minecraft.client.gui.GuiGraphics
-import net.minecraft.client.gui.components.AbstractWidget
 import net.minecraft.client.gui.layouts.FrameLayout
 import net.minecraft.client.gui.layouts.Layout
 import net.minecraft.client.gui.layouts.LayoutElement
@@ -29,7 +23,6 @@ import net.minecraft.gametest.framework.TestEnvironmentDefinition.Weather
 import net.minecraft.network.chat.Component
 import net.minecraft.resources.Identifier
 import net.minecraft.world.level.biome.Biome
-
 
 class ConfigScreen : Overlay(null) {
     private var screen: Layout? = null
@@ -65,123 +58,64 @@ class ConfigScreen : Overlay(null) {
 
         var weatherChangerFrameHeight = 30
 
-        val weatherChangerMainToggle = Widgets.labelled(
-            mc.font,
-            Component.translatable("config.snowyspirits.weatherChanger"),
-            createColoredToggleButton(
-                { Config.weatherChanger },
-                "Weather Changer",
-            ) { newVal ->
-                Config.weatherChanger = newVal
-                this.rebuildWidgets()
-            }
-        ).withEqualSpacing(Orientation.HORIZONTAL)
-
         var weatherChangerWeatherType: LayoutElement? = null
         var weatherChangerPrecipitationType: LayoutElement? = null
         var lightningChance: LayoutElement? = null
 
         if (Config.weatherChanger) {
             weatherChangerFrameHeight += 25
-            weatherChangerWeatherType = Widgets.labelled(
-                mc.font,
-                Component.translatable("config.snowyspirits.weatherType"),
-                Widgets.dropdown(
-                    weatherDropdownState,
-                    Weather.Type.entries,
-                    { Component.literal(it.name.toTitleCase()) },
-                    {
-                        it.withSize(100, 20).withTexture(UIConstants.BUTTON)
-                    }
-                ) {
-                    it.withSize(100, 20 * Weather.Type.entries.size + 4)
-                        .withTexture(UIConstants.MODAL_INSET)
-                        .withCallback { newVal ->
-                            Config.weatherType = newVal
-                        }
-                }
-            ).withEqualSpacing(Orientation.HORIZONTAL)
+            weatherChangerWeatherType = labeledDropdown(
+                "config.snowyspirits.weatherType",
+                weatherDropdownState,
+                Weather.Type.entries,
+            ) { Config.weatherType = it }
 
             weatherChangerFrameHeight += 25
-            weatherChangerPrecipitationType = Widgets.labelled(
-                mc.font,
-                Component.translatable("config.snowyspirits.precipitation"),
-                Widgets.dropdown(
-                    precipitationDropdownState,
-                    Biome.Precipitation.entries,
-                    { Component.literal(it.name.lowercase().replaceFirstChar { char -> char.uppercase() }) },
-                    {
-                        it.withSize(100, 20).withTexture(UIConstants.BUTTON)
-                    }
-                ) {
-                    it.withSize(100, 20 * Biome.Precipitation.entries.size + 4)
-                        .withTexture(UIConstants.MODAL_INSET)
-                        .withCallback { newVal ->
-                            Config.precipitation = newVal
-                        }
-                }
-            ).withEqualSpacing(Orientation.HORIZONTAL)
+            weatherChangerPrecipitationType = labeledDropdown(
+                "config.snowyspirits.precipitation",
+                precipitationDropdownState,
+                Biome.Precipitation.entries,
+                { Component.literal(it.name.lowercase().replaceFirstChar { char -> char.uppercase() }) }
+            ) { Config.precipitation = it }
 
             if (Config.weatherType == Weather.Type.THUNDER) {
                 weatherChangerFrameHeight += 25
-                lightningChance = Widgets.labelled(
-                    mc.font,
-                    Component.translatable("config.snowyspirits.lightningChance"),
-                    SliderWidget(
-                        lightningChanceState,
-                        0,
-                        1_000_000
-                    ).withSize(100, 20)
-                ).withEqualSpacing(Orientation.HORIZONTAL)
+                lightningChance = labeledSlider(
+                    "config.snowyspirits.lightningChance",
+                    lightningChanceState,
+                    0,
+                    1_000_000
+                )
             }
         }
 
-        val noPrecipitationBlockingToggle = Widgets.labelled(
-            mc.font,
-            Component.translatable("config.snowyspirits.noPrecipitationBlocking"),
-            createColoredToggleButton(
-                { Config.noPrecipitationBlocking },
-                "Weather Changer",
-            ) { newVal ->
-                Config.noPrecipitationBlocking = newVal
-            }
-        ).withEqualSpacing(Orientation.HORIZONTAL)
+        val noPrecipitationBlockingToggle = labeledToggle(
+            "config.snowyspirits.noPrecipitationBlocking",
+            { Config.noPrecipitationBlocking },
+            "Precipitation Blocking"
+        ) { newVal ->
+            Config.noPrecipitationBlocking = newVal
+        }
 
-        val weatherChangerVerticalLayout = Layouts.column()
-            .withGap(5)
-            .withChild(weatherChangerMainToggle)
-        if (weatherChangerWeatherType != null) weatherChangerVerticalLayout.withChild(weatherChangerWeatherType)
-        if (weatherChangerPrecipitationType != null) weatherChangerVerticalLayout.withChild(weatherChangerPrecipitationType)
-        if (lightningChance != null) weatherChangerVerticalLayout.withChild(lightningChance)
+        val weatherInnerContent = Layouts.column().withGap(5).apply {
+            if (weatherChangerWeatherType != null) this.withChild(weatherChangerWeatherType)
+            if (weatherChangerPrecipitationType != null) this.withChild(weatherChangerPrecipitationType)
+            if (lightningChance != null) this.withChild(lightningChance)
+        }
 
         var timeChangerFrameHeight = 30
-
-        val timeChangerMainToggle = Widgets.labelled(
-            mc.font,
-            Component.translatable("config.snowyspirits.timeChanger"),
-            createColoredToggleButton(
-                { Config.timeChanger },
-                "Time Changer",
-            ) { newVal ->
-                Config.timeChanger = newVal
-                this.rebuildWidgets()
-            }
-        ).withEqualSpacing(Orientation.HORIZONTAL)
 
         var timeInput: LayoutElement? = null
         var timePreset: LayoutElement? = null
 
         if (Config.timeChanger) {
             timeChangerFrameHeight += 25
-            timeInput = Widgets.labelled(
-                mc.font,
-                Component.translatable("config.snowyspirits.time"),
-                SliderWidget(
-                    timeState,
-                    0,
-                    24000
-                ).withSize(100, 20)
-            ).withEqualSpacing(Orientation.HORIZONTAL)
+            timeInput = labeledSlider(
+                "config.snowyspirits.time",
+                timeState,
+                0,
+                24000
+            )
 
             timeChangerFrameHeight += 29
             timePreset = Widgets.radio(
@@ -211,40 +145,26 @@ class ConfigScreen : Overlay(null) {
             ) { }
         }
 
-        val timeChangerVerticalLayout = Layouts.column()
+        val timeInnerContent = Layouts.column()
             .withGap(5)
-            .withChild(timeChangerMainToggle)
-        if (timeInput != null) timeChangerVerticalLayout.withChild(timeInput)
-        if (timePreset != null) timeChangerVerticalLayout.withChild(timePreset)
+            .apply {
+                if (timeInput != null) this.withChild(timeInput)
+                if (timePreset != null) this.withChild(timePreset)
+            }
 
         var moonChangerFrameHeight = 30
-
-        val moonChangerMainToggle = Widgets.labelled(
-            mc.font,
-            Component.translatable("config.snowyspirits.moonPhaseChanger"),
-            createColoredToggleButton(
-                { Config.moonPhaseChanger },
-                "Moon Changer",
-            ) { newVal ->
-                Config.moonPhaseChanger = newVal
-                this.rebuildWidgets()
-            }
-        ).withEqualSpacing(Orientation.HORIZONTAL)
 
         var moonPhaseWidget: LayoutElement? = null
         var moonRenderer: LayoutElement? = null
 
         if (Config.moonPhaseChanger) {
             moonChangerFrameHeight += 25
-            moonPhaseWidget = Widgets.labelled(
-                mc.font,
-                Component.translatable("config.snowyspirits.moonPhase"),
-                UnlabeledSliderWidget(
-                    moonPhaseState,
-                    0,
-                    Config.MoonPhase.entries.size - 1
-                ).withSize(100, 20)
-            ).withEqualSpacing(Orientation.HORIZONTAL)
+            moonPhaseWidget = labeledUnlabeledSlider(
+                "config.snowyspirits.moonPhase",
+                moonPhaseState,
+                0,
+                Config.MoonPhase.entries.size - 1
+            )
 
             moonChangerFrameHeight += 69
             moonRenderer = Widgets.renderable { graphics, context, _ ->
@@ -260,114 +180,75 @@ class ConfigScreen : Overlay(null) {
             }
         }
 
-        val moonChangerVerticalLayout = Layouts.column()
-            .withGap(5)
-            .withChild(moonChangerMainToggle)
-        if (moonPhaseWidget != null) moonChangerVerticalLayout.withChild(moonPhaseWidget)
-        if (moonRenderer != null) moonChangerVerticalLayout.withChild(moonRenderer)
-
+        val moonInnerContent = columnWithChildren(moonPhaseWidget, moonRenderer)
 
         var skyTypeFrameHeight = 30
-
-        val skyTypeChangerMainToggle = Widgets.labelled(
-            mc.font,
-            Component.translatable("config.snowyspirits.skyTypeChanger"),
-            createColoredToggleButton(
-                { Config.skyTypeChanger },
-                "Sky Type Changer",
-            ) { newVal ->
-                Config.skyTypeChanger = newVal
-                this.rebuildWidgets()
-            }
-        ).withEqualSpacing(Orientation.HORIZONTAL)
-
         var skyTypeWidget: LayoutElement? = null
 
         if (Config.skyTypeChanger) {
             skyTypeFrameHeight += 25
-            skyTypeWidget = Widgets.labelled(
-                mc.font,
-                Component.translatable("config.snowyspirits.skyType"),
-                Widgets.dropdown(
-                    skyTypeState,
-                    Config.SkyType.entries,
-                    { Component.literal(it.name.toTitleCase()) },
-                    {
-                        it.withSize(100, 20).withTexture(UIConstants.BUTTON)
-                    }
-                ) {
-                    it.withSize(100, 20 * Config.SkyType.entries.size + 4)
-                        .withTexture(UIConstants.MODAL_INSET)
-                        .withCallback { newVal ->
-                            Config.skyType = newVal
-                        }
-                }
-            ).withEqualSpacing(Orientation.HORIZONTAL)
+            skyTypeWidget = labeledDropdown(
+                "config.snowyspirits.skyType",
+                skyTypeState,
+                Config.SkyType.entries,
+            ) { Config.skyType = it }
         }
 
-        val skyChangerVertivalLayout = Layouts.column()
-            .withGap(5)
-            .withChild(skyTypeChangerMainToggle)
-        if (skyTypeWidget != null) skyChangerVertivalLayout.withChild(skyTypeWidget)
+        val skyInnerContent = columnWithChildren(skyTypeWidget)
 
         when (tab) {
             0 -> {
-                mainScrollableList.add(
-                    Widgets.frame()
-                        .withSize(mainScrollableList.width, weatherChangerFrameHeight)
-                        .withTexture(UIConstants.MODAL_INSET)
-                        .withContents {
-                            it.addChild(weatherChangerVerticalLayout)
-                        }
-                        .withContentFill()
-                        .withContentMargin(5)
-                )
+                ConfigSection(
+                    "config.snowyspirits.weatherChanger",
+                    "config.snowyspirits.weatherChanger.toggle",
+                    enabledGetter = { Config.weatherChanger },
+                    onToggle = { newVal ->
+                        Config.weatherChanger = newVal
+                        this.rebuildWidgets()
+                    },
+                    contentBuilder = { weatherInnerContent },
+                    explicitHeight = weatherChangerFrameHeight
+                ).addTo(mainScrollableList)
 
-                mainScrollableList.add(
-                    Widgets.frame()
-                        .withSize(mainScrollableList.width, 30)
-                        .withTexture(UIConstants.MODAL_INSET)
-                        .withContents {
-                            it.addChild(noPrecipitationBlockingToggle)
-                        }
-                        .withContentFill()
-                        .withContentMargin(5)
-                )
+                addFrameToList(mainScrollableList, noPrecipitationBlockingToggle, 30)
             }
             1 -> {
-                mainScrollableList.add(
-                    Widgets.frame()
-                        .withSize(mainScrollableList.width, timeChangerFrameHeight)
-                        .withTexture(UIConstants.MODAL_INSET)
-                        .withContents {
-                            it.addChild(timeChangerVerticalLayout)
-                        }
-                        .withContentFill()
-                        .withContentMargin(5)
-                )
+                ConfigSection(
+                    "config.snowyspirits.timeChanger",
+                    "config.snowyspirits.timeChanger.toggle",
+                    enabledGetter = { Config.timeChanger },
+                    onToggle = { newVal ->
+                        Config.timeChanger = newVal
+                        this.rebuildWidgets()
+                    },
+                    contentBuilder = { timeInnerContent },
+                    explicitHeight = timeChangerFrameHeight
+                ).addTo(mainScrollableList)
 
-                mainScrollableList.add(
-                    Widgets.frame()
-                        .withSize(mainScrollableList.width, moonChangerFrameHeight)
-                        .withTexture(UIConstants.MODAL_INSET)
-                        .withContents {
-                            it.addChild(moonChangerVerticalLayout)
-                        }
-                        .withContentFill()
-                        .withContentMargin(5)
-                )
+                ConfigSection(
+                    "config.snowyspirits.moonPhaseChanger",
+                    "config.snowyspirits.moonPhaseChanger.toggle",
+                    enabledGetter = { Config.moonPhaseChanger },
+                    onToggle = { newVal ->
+                        Config.moonPhaseChanger = newVal
+                        this.rebuildWidgets()
+                    },
+                    contentBuilder = { moonInnerContent },
+                    explicitHeight = moonChangerFrameHeight
+                ).addTo(mainScrollableList)
             }
             2 -> {
-                mainScrollableList.add(
-                    Widgets.frame()
-                        .withSize(mainScrollableList.width, skyTypeFrameHeight)
-                        .withTexture(UIConstants.MODAL_INSET)
-                        .withContents {
-                            it.addChild(skyChangerVertivalLayout)
-                        }
-                        .withContentFill()
-                        .withContentMargin(5)
-                )
+                ConfigSection(
+                    titleKey = "config.snowyspirits.skyTypeChanger",
+                    toggleKey = "config.snowyspirits.skyTypeChanger.toggle",
+                    enabledGetter = { Config.skyTypeChanger },
+                    onToggle = { newVal ->
+                        Config.skyTypeChanger = newVal
+                        this.rebuildWidgets()
+                    },
+                    contentBuilder = { skyInnerContent },
+                    explicitHeight = skyTypeFrameHeight
+                ).addTo(mainScrollableList)
             }
             else -> {}
         }
@@ -453,43 +334,6 @@ class ConfigScreen : Overlay(null) {
         FrameLayout.centerInRectangle(this.screen!!, this.rectangle)
     }
 
-    fun <T : AbstractWidget> texture(identifier: Identifier, width: Int = 16, height: Int = 16, yOffset: Int = -1) : WidgetRenderer<T> {
-        return WidgetRenderer { graphics: GuiGraphics, context: WidgetRendererContext<T>, _: Float ->
-            graphics.blit(
-                RenderPipelines.GUI_TEXTURED,
-                identifier,
-                context.x + (context.width - width) / 2,
-                context.y + (context.height - height) / 2 + yOffset,
-                0f, 0f,
-                width, height,
-                width, height,
-            )
-        }
-    }
-
-    fun createColoredToggleButton(
-        configBool: () -> Boolean,
-        label: String,
-        onToggle: (Boolean) -> Unit
-    ): BaseWidget {
-        return Widgets.button()
-            .withSize(20, 20)
-            .withTexture(UIConstants.BUTTON)
-            .withCallback {
-                onToggle(!configBool())
-            }
-            .withRenderer { graphics, context, _ ->
-                graphics.fill(
-                    context.x + 2,
-                    context.y + 2,
-                    context.x + context.width - 2,
-                    context.y + context.height - 4,
-                    if (configBool()) 0x7755FF55 else 0x77FF5555
-                )
-            }
-            .withTooltip(Component.literal(label))
-    }
-
     fun getTimeFromString(preset: String): Long {
         return when (preset) {
             "Sunrise" -> 0L
@@ -524,9 +368,5 @@ class ConfigScreen : Overlay(null) {
     override fun onClose() {
         SnowySpirits.saveConfig()
         super.onClose()
-    }
-
-    fun String.toTitleCase(): String {
-        return this.lowercase().replaceFirstChar { char -> char.uppercase() }
     }
 }
